5e0a9f0c143ab2dc4f0893645db1a0fe46387faa
antoniosalvadorcosta <asb.costa@campus.fct.unl.pt>
2025-01-22 19:03:37 +0000

    eliminação de lixo

--- shuttle/b.m
+++
@@ -1,9 +0,0 @@
-% Rearrange the matrix into a 4x4 matrix
-
-disp(rmse_values_wind);
-rearrangedMatrix = reshape(rmse_values_wind, 4, 12);
-
-rmse_values_wind = round(rearrangedMatrix, 3);
-
-% Write the numerical data to Excel spreadsheet using writetable
-writematrix(rmse_values_wind);f & %f & %f\n',rmse_values(4,1),rmse_values(4,2),rmse_values(4,3),rmse_values(4,4));
